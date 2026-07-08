17cacd2da0f60734b6778b64fc14719ea252f02f
marlonagner <marlon.agner@gmail.com>
2026-07-08 15:07:40 -0300

    Implementando while e break

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-print("\nMenu Inicial:")
-print("1.Cadastrar Cliente")
-print("2. Listar Cliente")
-print("3. Alterar Cliente")
-print("4. Remover Cliente")
-print("5. Sair")
+while True:
+ print("\nMenu Inicial:")
+ print("1.Cadastrar Cliente")
+ print("2. Listar Cliente")
+ print("3. Alterar Cliente")
+ print("4. Remover Cliente")
+ print("5. Sair")
+ opcao = input("Escolha uma opcao: ")
 
-opcao = input("Escolha uma opcao: ")
-if opcao == "1":
+ if opcao == "1":
     print("Opcao de Cadastrar Cliente selecionada.")
-elif opcao == "2":
-    print("Opcao de Listar Clientes selecionada.")
-elif opcao == "3":
-    print ("Opcao de Alterar Clientes selecionados")
-elif opcao == "4":
-    print("Opcao de Remover clientes selecionada")
-else:
-    print("Saindo do sistema. See Ya!")
+ elif opcao == "2":
+        print("Opcao de Listar Clientes selecionada.")
+ elif opcao == "3":
+        print ("Opcao de Alterar Clientes selecionados")
+ elif opcao == "4":
+        print("Opcao de Remover clientes selecionada")
+ elif opcao == "5":
+        print("Saindo do sistema. See Ya!")
+        break
+ else:
+        print("A Opcao escolhida nao existe. Por favor, tente novamente.")
